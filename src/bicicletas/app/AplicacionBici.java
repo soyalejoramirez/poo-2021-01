e55702c034caf47edc0e3bb9b7c8750948138913
Alejandro Ramírez <alejandroramirez@saimyr.com.co>
2021-03-09 15:26:30 -0500

    Garbage Collector

--- src/bicicletas/app/AplicacionBici.java
+++ src/bicicletas/app/AplicacionBici.java
@@ -3,17 +3,46 @@ package bicicletas.app;
 import bicicletas.dominio.Bicicleta;
 
 public class AplicacionBici {
-    public static void main(String[] args) {
-        Bicicleta bici = new Bicicleta(544813, "Trek", "Marlin 7 2021", "Blanca", 3200000);
+    public static void main(String[] args) throws InterruptedException {
+        Bicicleta bici = null;
 
-        bici.acelerar(35.2);
-        bici.frenar(18.2);
+        for(int i = 0; i < 999999999; i++) {
+            bici = new Bicicleta(544813, "Cannondale", "Madone 2021", "Verde", 10000000);
+            bici = null;
+        }
 
-        System.out.println(bici.obtenerInformacion());
+        System.out.println("Memoria libre después de terminar " + Runtime.getRuntime().freeMemory());
+        System.gc();
+        System.out.println("Memoria libre después del Garbage Collector " + Runtime.getRuntime().freeMemory());
 
-        bici.setPrecio(3500000);
-        bici.setColor("Esmerald Iris");
+//        Bicicleta otraBici = null;
+//
+//        System.out.println(bici);
+//        System.out.println(otraBici);
+//        System.out.println("\n");
+//        otraBici = new Bicicleta(544813, "Trek", "Marlin 7 2021", "Blanca", 3200000);
+//
+//        System.out.println(bici);
+//        System.out.println(otraBici);
+//
+//        System.out.println("\n");
+//        System.out.println(bici.getColor());
+//        System.out.println(otraBici.getColor());
+//
+//        System.out.println("\n");
+//        System.out.println("Vamos a cambiar el color de otraBici");
+//        otraBici.setColor("Negra");
+//
+//        System.out.println(bici.getColor());
 
-        System.out.println(bici.obtenerInformacion());
+//        bici.acelerar(35.2);
+//        bici.frenar(18.2);
+//
+//        System.out.println(bici.obtenerInformacion());
+//
+//        bici.setPrecio(3500000);
+//        bici.setColor("Esmerald Iris");
+//
+//        System.out.println(bici.obtenerInformacion());
     }
 }
